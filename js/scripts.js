6b8eb1e506e09f9a07da35a096e28070ff804cb1
Rafal <rbagrowsk@gmail.com>
2018-03-17 19:38:36 +0000

    Feat: Add star rating system

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@
 const iconList = ["address-book","address-card","adjust","archive","balance-scale","baseball-ball","basketball-ball","bed","birthday-cake","blind","bolt","book","bookmark","bowling-ball","briefcase","building","bullhorn","bullseye","calculator","calendar","calendar-alt","camera","camera-retro","certificate","chart-area","chart-bar","chart-line","chart-pie","child","circle","clipboard","clone","cloud","coffee","columns","comment","compass","compress","copy","copyright","crop","crosshairs","cut","desktop","download","edit","envelope","envelope-open","envelope-square","eraser","expand","eye","eye-dropper","eye-slash","fax","female","file","file-alt","file-image","film","folder","folder-open","football-ball","frown","futbol","globe","golf-ball","hdd","headphones","heart","hockey-puck","id-badge","id-card","image","images","industry","keyboard","laptop","male","map-marker","meh","microchip","mobile","mobile-alt","object-group","object-ungroup","paint-brush","paperclip","paste","pen-square","pencil-alt","percent","phone","phone-square","phone-volume","play","plug","power-off","print","quidditch","registered","save","server","sitemap","sliders-h","smile","square","star","sticky-note","street-view","suitcase","table","table-tennis","tablet","tablet-alt","tag","tags","tasks","thumbtack","tint","trademark","tv","upload","user","user-circle","user-md","user-plus","user-secret","user-times","users","volleyball-ball","wheelchair"];
 const cardDeck = document.querySelector('.card-deck');
 let selectedCards = [];
+let moves = 0;
 let clockInterval;
 
 function initializeGame() {
@@ -33,12 +34,12 @@ function shuffleSelectedIcons(selectedIcons) {
 function appendCardsToHTML(cardArray) {
 	const cardContainer = document.createDocumentFragment();
 	cardArray.forEach((el) => {
-			let cardDiv = document.createElement('div');
-			let card = document.createElement('i');
-			cardDiv.className = "card";
-			card.className = `fas fa-${el}`;
-			cardDiv.appendChild(card);
-			cardContainer.appendChild(cardDiv);
+		let cardDiv = document.createElement('div');
+		let card = document.createElement('i');
+		cardDiv.className = "card";
+		card.className = `fas fa-${el}`;
+		cardDiv.appendChild(card);
+		cardContainer.appendChild(cardDiv);
 	});
 	cardDeck.appendChild(cardContainer);
 }
@@ -67,9 +68,6 @@ function resetClock() {
 }
 ///////////
 
-
-
-
 function initializeEventListeners() {
 	const cards = document.querySelectorAll('.card');
 
@@ -83,8 +81,11 @@ function initializeEventListeners() {
 				if(selectedCards.length === 2) {
 					selectedCards[0] === selectedCards[1] ? colorMatchedCards(true) : colorMatchedCards(false);
 					selectedCards = [];
+					moves += 1;
+					updateRating(moves);
+					document.querySelector('.moves').innerText = "Moves: " + moves;
 				}
-					checkForWin();
+				checkForWin();
 			}
 		});
 	});
@@ -105,12 +106,30 @@ function restoreIncorrectCards() {
 	setTimeout(() => selection.forEach(el => el.className = "card"),1500);
 }
 
+function updateRating(moves) {
+	const STEP = 10;
+	const stars = document.querySelectorAll('svg.fa-star.gold');
+	const lastStar = stars[stars.length-1];
+	//3 cases, since we have 3 stars
+	switch (moves) {
+		case STEP :
+			lastStar.classList.remove('gold');
+		break;
+		case STEP * 2:
+			lastStar.classList.remove('gold');
+		break;
+		case STEP * 3:
+			lastStar.classList.remove('gold');
+		break;
+	}
+}
+
 function checkForWin() {
 	const allCards = document.querySelectorAll('.card').length;
 	const correctCards = document.querySelectorAll('.correct').length || 0;
 	if (correctCards === allCards) {
 		console.log('win!');
-		clearInterval(clockInterval);
+		stopClock();
 	}
 }
 
